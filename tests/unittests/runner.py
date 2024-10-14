1ef6294b5cc9b3b258d6cbec41e1e89d29bee749
Alexander Engström <123507241+alexandengstrom@users.noreply.github.com>
2024-10-14 23:23:34 +0200

    Update runner.py

--- tests/unittests/runner.py
+++ tests/unittests/runner.py
@@ -1,9 +1,13 @@
 import os
 import unittest
+import sys
 
 os.environ['birchrest_log_level'] = 'test'
 
 loader = unittest.TestLoader()
 tests = loader.discover('.')
 testRunner = unittest.TextTestRunner()
-testRunner.run(tests)
+result = testRunner.run(tests)
+
+if not result.wasSuccessful():
+    sys.exit(1)
